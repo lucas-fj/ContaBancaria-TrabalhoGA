6b26448945dce4cbee60015e34e14013b7da3c92
yasmimakati123-boop <yasmimakati123@gmail.com>
2026-04-10 19:49:27 -0300

    ContaCorrente (S/D).java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -22,10 +22,57 @@ public class ContaCorrente extends ContaBancaria {
 
     //método movimenta (faz apenas as operações de depósito e saque verificando se o saldo está disponível e o limite ok)
     public void movimenta(Operacao op){
-        if (op.getTipo() == 'D') { //depósito
-            saldo += op.getValor();
-            depositos.registrarMovimentacao(op.getValor());
-            System.out.println("Novo saldo: R$" + saldo);
+        if (op.getTipo() == 'D') { //depósito  
+        saldo += valor;
+
+        // registra depósito
+        depositos.setQuantidade(depositos.getQuantidade() + 1);
+        depositos.setValorTotal(depositos.getValorTotal() + valor);
+
+        System.out.printf("Depósito realizado! Novo saldo: R$ %.2f\n", saldo);
+    }
+    else if (op.getTipo() == 'S') {//saque
+
+        // verificar saldo
+        if (valor > saldo) {
+            System.out.println("Saldo insuficiente!");
+            return;
+        }
+
+        int[] notas = {100, 50, 20, 10, 5, 2};
+        int[] qtdNotas = new int[notas.length];
+
+        double restante = valor;
+
+        for (int i = 0; i < notas.length; i++) {
+            qtdNotas[i] = (int) (restante / notas[i]);
+            restante = restante % notas[i];
+        }
+
+        // não consegue formar valor
+        if (restante != 0) {
+            System.out.println("Não é possível sacar esse valor com as notas disponíveis.");
+            return;
+        }
+
+        // mostrar notas
+        System.out.println("\nNotas entregues:");
+        for (int i = 0; i < notas.length; i++) {
+            if (qtdNotas[i] > 0) {
+                System.out.println(qtdNotas[i] + " nota(s) de R$" + notas[i]);
+            }
+        }
+
+        // atualizar saldo
+        saldo -= valor;
+
+        // registrar saque
+        saques.setQuantidade(saques.getQuantidade() + 1);
+        saques.setValorTotal(saques.getValorTotal() + valor);
+
+        System.out.printf("Saque realizado! Novo saldo: R$ %.2f\n", saldo);
+    }
+
         }
     }
 }
